181a6461b389a7997bff0a1643e4576b0fe15da5
Andy <abc623979@gmail.com>
2019-02-11 17:03:34 +0800

    LocationVO

---
+++ src/model/LocationVO.java
@@ -0,0 +1,24 @@
+package model;
+
+public class LocationVO {
+	private String memId;
+	private String location;
+
+	public LocationVO() {}
+
+	public String getMemId() {
+		return memId;
+	}
+
+	public void setMemId(String memId) {
+		this.memId = memId;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+}
